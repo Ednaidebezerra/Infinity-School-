47ddcb9dbe6850a6aeca789b7ed326a2a9d4e2bf
Ednaidebezerra <ednabezerra2022@outlook.com>
2025-12-03 12:10:22 -0300

    .

--- Pyton/aula-4/prova/funcao.py
+++ Pyton/aula-4/prova/funcao.py
@@ -1,20 +1,9 @@
-
-def media (n1, n2, n3):
-    """
-    Calcula a média aritmética de três números.
-
-    Parâmetros:
-    n1 (float): O primeiro número.
-    n2 (float): O segundo número.
-    n3 (float): O terceiro número.
-
-    Retorna:
-    float: A média aritmética dos três números.
-    """
-    return (n1 + n2 + n3) / 3   
+# Define a função que calcula a média de três números
+def media (m1, m2, m3):
+    return (m1 + m2 + m3) / 3 
+  
 # Solicita ao usuário que insira três números
-
 entradas = input("Digite três números separados por espaço: ")
 valores = entradas.split()
-n1, n2, n3 = map(float, valores)
-print(f"A sua media é {media(n1, n2, n3):.2f}")
+m1, m2, m3 = map(float, valores)
+print(f"A sua media é {media(m1, m2, m3):.2f}")
