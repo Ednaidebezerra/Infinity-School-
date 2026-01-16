d07beffffdc272cfa12862312bce661e33f69948
Ednaidebezerra <ednabezerra2022@outlook.com>
2026-01-16 10:15:44 -0300

    prova-2

---
+++ Lógica de Programação/aula-2/prova-2.py
@@ -0,0 +1,10 @@
+
+## identifica o numero é positivo, negativo ou 0, usando F-strings; 
+
+numero = int(input("Digite um número: "))
+if numero > 0:
+    print(f"O número {numero} é positivo.")
+elif numero < 0:
+    print(f"O número {numero} é negativo.")
+else:
+    print(f"O número {numero} é zero.")
