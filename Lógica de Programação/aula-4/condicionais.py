c949b7bebc1dfa46bf636c1d46b694c64daed6bb
Ednaidebezerra <ednabezerra2022@outlook.com>
2026-01-16 16:02:33 -0300

    Create condicionais.py

---
+++ Lógica de Programação/aula-4/condicionais.py
@@ -0,0 +1,15 @@
+
+## Range 
+
+range(10)  # Gera números de 0 a 9
+range(5, 15)  # Gera números de 5 a 14
+range(0, 20, 5)  # Gera números de 0 a 19, pulando de 5 em 5
+
+## Len
+
+len("Hello")  # Retorna 5 quantidade de caracteres na string
+len([1, 2, 3, 4, 5])  # Retorna 5 quantidade de elementos na lista  
+
+## .append()
+lista = [1, 2, 3]
+lista.append(4)  # Adiciona o elemento 4 ao final da lista
