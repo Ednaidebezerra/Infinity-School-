60f42d80d844a1b28c4176413b7610f877cde5a0
Ednaidebezerra <ednabezerra2022@outlook.com>
2026-01-16 16:25:12 -0300

    prova-4

---
+++ Lógica de Programação/aula-4/prova-4.py
@@ -0,0 +1,15 @@
+
+## soma de numeros pares no intervalo de numeros digitados pelo usuario.
+
+inicio = int(input("Digite o número inicial do intervalo: "))
+fim = int(input("Digite o número final do intervalo: "))
+
+soma = 0
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+
+if soma == 0:
+    print("Não há números pares no intervalo.")
+else:
+    print("A soma dos números pares no intervalo é:", soma)
